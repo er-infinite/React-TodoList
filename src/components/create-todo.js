9009b78ab0cb44c65bba37d28fcb0ae59d0b48b7
Emma <elr9@njit.edu>
2018-04-24 01:27:31 -0400

    Create button now logs dom element

--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -3,11 +3,16 @@ import React from 'react';
 export default class CreateTodo extends React.Component{
     render(){
         return(
-            <form>
-                <input type="text" placeholder="To Do " />
+            <form onSubmit={this.handleCreate.bind(this)}>
+                <input type="text" placeholder="To Do " ref="createInput"/>
                 <button>Create</button>
             </form>
         );
+    }
+
+    handleCreate(event){
+        event.preventDefault();
+        console.log(this.refs.createInput);
     }
 }
 
