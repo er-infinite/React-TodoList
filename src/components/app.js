93e45c2c558169bad9b1b6db4ae8689a31ae7703
Emma <elr9@njit.edu>
2018-04-26 00:38:15 -0400

    error handling fixed problem when adding duplicate tasks and when nothing is inputed when creating a task

--- src/components/app.js
+++ src/components/app.js
@@ -29,7 +29,7 @@ export default class App extends React.Component{
         return(
             <div>
                 <h1>React To Do App</h1>
-                <CreateTodo createTask={this.createTask.bind(this)}/>
+                <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)}/>
                 <TodoList
                     todos={this.state.todos}
                     toggleTask={this.toggleTask.bind(this)}
--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -1,21 +1,60 @@
 import React from 'react';
 
 export default class CreateTodo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error: null
+        }
+    }
+
+    renderError(){
+        if(!this.state.error){ return null;}
+
+        return <div style={{color: 'red'}}>{this.state.error}</div>;
+    }
+
     render(){
         return(
             <form onSubmit={this.handleCreate.bind(this)}>
                 <input type="text" placeholder="To Do " ref="createInput"/>
                 <button>Create</button>
+                {this.renderError()}
             </form>
         );
     }
 
     handleCreate(event){
-        event.preventDefault();
+        event.preventDefault()
+
+        const createInput = this.refs.createInput;
+        const task = createInput.value;
+        const validateInput = this.validateInput(task);
+
+        //if validateInput returned a message
+        if(validateInput){
+            this.setState({error: validateInput});
+            return;
+        }
+
+        //if no error, reset state to null
+        this.setState({error: null});
+
         // console.log(this.refs.createInput.value);
         // console.log(this.props.createTask); //debugging creating task
-        this.props.createTask(this.refs.createInput.value);
+        this.props.createTask(task);
         this.refs.createInput.value = '';
+    }
+
+    //If there is no task, then error
+    validateInput(task) {
+        if (!task) {
+            return "Enter a Task";
+        } else if (_.find(this.props.todos, todo => todo.task === task)) { //searches todos in form <CreateTodo> app.js
+            return "Duplicate Task";
+        }else {
+            return null; //return null, or no error
+        }
     }
 }
 
